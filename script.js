79f488ac1c431e780d86ceb4f32c0f282aa33137
Suddukumar <138306342+Suddukumar@users.noreply.github.com>
2023-08-06 15:45:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// const item=document.querySelector("#item");
+// const todobox = document.querySelector("#to-do-box");
+
+// item.addEventListener(
+//     "keyup",
+//     function(event){
+//         if (event.key == "enter") {
+//             addtodo(this.value)
+//             this.value= ""
+
+            
+//         }
+//     }
+// )
+// const addtodo=(item)=>{
+//     const listitem = document.createElement("li");
+//     listItem.innerHTML=`
+//     ${item}
+//     <i class="fas fa-times"></i>`;
+
+//     todobox.appendChild(listItem)
+// }
+const item = document.querySelector("#item")
+const toDoBox = document.querySelector("#to-do-box")
+
+item.addEventListener(
+  "keyup",
+  function(event) {
+    if (event.key == "Enter") {
+      addToDo(this.value)
+      this.value = ""
+    }
+  }
+)
+
+const addToDo = (item) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `
+         ${item}
+        <i class="fas fa-times"></i>
+    `;
+
+  listItem.addEventListener(
+    "click",
+    function() {
+      this.classList.toggle("done");
+    }
+  )
+  listItem.querySelector("i").addEventListener(
+    "click",
+    function() {
+      listItem.remove()
+    }
+  )
+  toDoBox.appendChild(listItem)
+}
